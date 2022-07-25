49ae0990e06c41f33917c6ec9672e6159b74194f
Zekpy <80614212+Zekpy@users.noreply.github.com>
2022-07-25 14:06:20 -0300

    Add files via upload

---
+++ Arreglos.java
@@ -0,0 +1,16 @@
+package Paquete;
+
+public class Arreglos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x []= {10,5,3,6};
+		
+		for (int i=0; i<=x.length;i++)
+		{
+			System.out.println("el numero del arreglo es "+i+" El valor es "+x[i]);
+		}
+		
+	}
+
+}
